2496130812851bebda90f2b7b41842a93d8c3b9e
OhJaeGeun <worms1133@naver.com>
2022-08-21 21:02:20 +0900

    first commit

---
+++ motor_control_kb/atmega328_key_control.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <wiringSerial.h>
+
+//#include <curses.h>
+//#include <conio.h>
+
+//#include "linux_kbhit.h"
+#include "my_conio.h"
+
+#define BAUD 115200
+
+#define UP 65
+#define DOWN 66
+#define LEFT 68
+#define RIGHT 67
+#define STOP 32
+
+int main(void)
+{
+	int fd;
+	char con;
+
+	init_keyboard();
+	//sleep(1);
+	//if((fd = serialOpen("/dev/ttyS0", BAUD)) < 0){
+	if((fd = serialOpen("/dev/ttyAMA1", BAUD)) < 0){
+		printf("Device file open error!! use sudo ...\n");
+		return 1;
+	}
+	printf("[UART test with Arduino board]\n");
+	serialPuts(fd, "[UART test with Arduino board]\n");
+
+	printf("start : please press the con\n");
+	//sleep(1);
+	con = 'S';
+	//sleep();
+	//printf("%d********\n",con); 
+	serialPutchar(fd, con);
+	//sleep(5);
+
+	while(1){
+		//printf("\nInput a arrow keys: ");
+
+		if(_kbhit()){
+			con = _getch();
+			//printf("%c******", con);
+			if(con == UP || con == DOWN || con == LEFT || con == RIGHT)
+				con = con;
+			else if(con == STOP)
+				con = 'S';
+			else
+				continue;
+			
+			printf("%d \n", con);
+			serialPutchar(fd, con);
+			
+			continue;
+			//sleep(0.5);
+			//serialFlush(fd);
+		}
+		//con = 'S';
+		//sleep();
+		//printf("%d********\n",con); 
+		//serialPutchar(fd, con);
+	}
+	close_keyboard();
+
+	return 0;
+}
---
+++ motor_driver_module_Lane/motor_driver_module_Lane/UART1.h
@@ -0,0 +1,18 @@
+﻿/*
+ * UART1.h
+ *
+ * Created: 2022-08-21 오후 8:52:57
+ *  Author: JAEGEUN
+ */ 
+
+
+#ifndef UART1_H_
+#define UART1_H_
+
+void UART_1_init(void); // 초기화
+void UART1_transmit(char data); // 송신
+unsigned char UART1_receive(void); // 수신
+
+
+
+#endif /* UART1_H_ */
